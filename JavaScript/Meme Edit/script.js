d575268dac5ce11e311865a18e3bcd50c3c545f4
Titus Thomas <tttitus99@gmail.com>
2025-02-24 21:19:15 +0530

    JS projects adding

---
+++ JavaScript/Meme Edit/script.js
@@ -0,0 +1,5 @@
+let drakePicTwo = document.getElementById("drake-pic-2");
+let heading = document.getElementById("heading-2");
+
+drakePicTwo.src = "Images/Drake Img2.png";
+heading.innerText = "Learn by doing";
